5537d94cce269ffd095f56ad98030552720c40b3
mbynmr <mbynmr@nottingham.ac.uk>
2025-05-29 22:35:27 +0100

    screwy local changes. added silly_func()

--- eloplot.py
+++ eloplot.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import requests
 import os.path
 import datetime as dt
+from tqdm import tqdm
 
 from fitting import fit
 
@@ -91,8 +92,14 @@ def plot_elo(plot_option, league, rulesiter, nameiter, min_elo, plot_oppelo, plo
                 case 'elo':
                     ax.plot([800, *elo[::-1]], '-', label=label)
                     if plot_oppelo:
-                        ax.scatter(range(len(elo) + 1), np.asarray([np.nan, *oppelo[::-1]]),
-                                   label='opp ' + label, s=np.array([1, *np.array((1 / 43) * delta ** 2)]))
+                        s = np.insert((1 / 43) * delta ** 2, 0, 1)
+                        ax.scatter(range(len(oppelo) + 1),
+                                   np.insert(np.where(result == 1, oppelo, np.nan)[::-1], 0, np.nan), color='g', s=s)
+                        ax.scatter(range(len(oppelo) + 1),
+                                   np.insert(np.where(result == -1, oppelo, np.nan)[::-1], 0, np.nan), color='r', s=s)
+                        ax.scatter(range(len(oppelo) + 1),
+                                   np.insert(np.where(result == 0, oppelo, np.nan)[::-1], 0, np.nan), color='b',
+                                   s=np.insert(np.abs((elo - oppelo))[::-1] / 10, 0, 1))
                         # todo separate into won/lost red/green (/draw) so the plot is clearer
                     if plot_fit != 0:
                         x = range(len([800, *elo[::-1]]))
@@ -105,7 +112,16 @@ def plot_elo(plot_option, league, rulesiter, nameiter, min_elo, plot_oppelo, plo
                     ax.plot(datex, elo, '-', label=label)
                     ax.plot(datex[-1], 800, 'ko')
                     if plot_oppelo:
-                        ax.scatter(datex, oppelo, label='opp ' + label, s=np.array((1 / 43) * delta[::-1] ** 2))
+                        s = np.array((1 / 43) * delta ** 2)
+
+                        # ax.scatter(datex, oppelo, label='opp ' + label, s=s[::-1])
+                        ax.scatter(datex,
+                                   np.where(result == 1, oppelo, np.nan), color='g', s=s)
+                        ax.scatter(datex,
+                                   np.where(result == -1, oppelo, np.nan), color='r', s=s)
+                        ax.scatter(datex,
+                                   np.where(result == 0, oppelo, np.nan), color='b',
+                                   s=np.abs((elo - oppelo)) / 10)
                 case 'co_pick,winrate':
                     categories = co_list_maker(rules)
                     entries = co_pick
@@ -119,19 +135,11 @@ def plot_elo(plot_option, league, rulesiter, nameiter, min_elo, plot_oppelo, plo
                     categories = range(int(np.amax(days) + 1))
                     entries = days
                 case 'days':
-                    wind = np.ones(len(days)) * np.nan
-                    losed = np.copy(wind)
-                    drawd = np.copy(wind)
-                    for i, d in enumerate(days[::-1]):
-                        if result[i] == 1:
-                            wind[i] = d
-                        elif result[i] == -1:
-                            losed[i] = d
-                        else:
-                            drawd[i] = d
-                    ax.scatter(range(len(days)), drawd, s=(elo - oppelo)[::-1] ** 2 / 500, color='b', label='draw')
-                    ax.scatter(range(len(days)), wind, s=(10 / 43) * delta ** 2, color='g', label='win')
-                    ax.scatter(range(len(days)), losed, s=(10 / 43) * delta ** 2, color='r', label='loss')
+                    s = (10 / 43) * delta ** 2
+                    ax.scatter(range(len(days)), np.where(result == 1, days, np.nan)[::-1], color='g', s=s)
+                    ax.scatter(range(len(days)), np.where(result == -1, days, np.nan)[::-1], color='r', s=s)
+                    ax.scatter(range(len(days)), np.where(result == 0, days, np.nan)[::-1], color='b',
+                               s=(elo - oppelo)[::-1] ** 2 / 500)
 
             # plot winrate plots (teeni bit of calcs to do)
             if plot_option.split(',')[-1] == 'winrate':  # figure out winrate in % for categories
@@ -161,12 +169,171 @@ def plot_elo(plot_option, league, rulesiter, nameiter, min_elo, plot_oppelo, plo
                            label=label + ', ' + str(int(np.sum(winc + losec))))
                 ax.scatter(x=categories, y=50 * np.ones(len(categories)), marker='.', c='k')
                 plt.ylim([0, 100])
+                plt.grid(visible=True)
                 plt.yticks(np.arange(11) * 10)  # np.linspace(start=0, stop=100, endpoint=True, num=11)
             # min_elo = 900
 
     plt.legend()
     plt.tight_layout()
+    # plt.grid()
     plt.show()
+
+
+
+# def silly func
+# all the GL CO pick data from all the profiles and plot aggro CO pick% vs rating
+def silly_func():
+    # x-axis: CO
+    # y-axis: rating after game
+    high_elo_cutoff = {'STD': 1300, 'FOG': 1300, 'HF': 1150}
+
+    # figure stuff
+    fig, ax = plt.subplots(1)
+    fig.autofmt_xdate()  # format the x-axis for squeezing in longer tick labels
+
+    for rules in ['STD', 'FOG', 'HF']:
+        # http://awbw.mooo.com/search?q=GL+STD+after+2025-05-18+rating%3E1000
+        s = f'GL+{rules}+after+2025-01-01+rating%3E{high_elo_cutoff[rules]}'
+        s = f'GL+{rules}+rating%3E{high_elo_cutoff[rules]}'
+
+        if not os.path.isfile('outputs/' + 'sillysearch' + rules + '.txt'):  # does the local file already exist?
+            scrape_silly(s, rules)
+
+        # analysis
+        categories = co_list_maker('agro')
+        days, winner, ratingw, ratingl, cow, col = retrieve_silly(rules)
+
+        # removers!
+        removers = np.argwhere(days == 0)
+        days[removers] = np.nan
+        winner[removers] = np.nan
+        ratingw[removers] = np.nan
+        ratingl[removers] = np.nan
+        # cow[removers] = np.nan
+        # col[removers] = np.nan
+
+        # plotting
+        casee = 'co,rating'  # co,win
+        match casee:
+            case 'co,rating':
+                # plot of CO on x, rating on y
+                avg_rating = 1
+                for i, co in enumerate(categories):
+                    avg_rating += i  # todo
+                ax.scatter(categories, avg_rating,
+                           s=10 * 100 * (winc + losec) / np.sum(winc + losec),
+                           label=rules + ', ' + str(int(np.sum(winc + losec))), alpha=0.7)
+            case 'co,win':
+                # plot of CO on x, win% on y
+                winc = np.zeros(len(categories))
+                losec = np.zeros(len(categories))
+                for i, co in enumerate(categories):
+                    m = 0
+                    for e in cow:
+                        if e == co:
+                            m += 1
+                    n = 0
+                    for e in col:
+                        if e == co:
+                            n += 1
+                    winc[i] = m
+                    losec[i] = n
+                    # winc[i] = len(np.argwhere(cow == co))
+                    # losec[i] = len(np.argwhere(col == co))
+
+                ax.scatter(categories, (winc / (winc + losec)) * 100,
+                           s=10 * 100 * (winc + losec) / np.sum(winc + losec),
+                           label=rules + ', ' + str(int(np.sum(winc + losec))), alpha=0.7)
+                plt.ylim([0, 100])
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def scrape_silly(search, rules):
+    s = "http://awbw.mooo.com/searchReplays.php?q=" + search  # &spoiler=on
+    page = page_getter(s)
+    resultbox = str(page.find("div", class_="resultBox").next.next)
+
+    # offsets = [1]
+    # while offsets[-1] + 200 <= int(resultbox.split(' ')[0]):
+    #     offsets.append(offsets[-1] + 200)
+    try:
+        offsets = [(e * 200) + 1 for e in range(int(np.ceil(int(resultbox.split(' ')[0]) / 200)))]
+    except ValueError as e:  # ValueError: invalid literal for int() with base 10: 'No'
+        print(f"search {search} had no results")
+        print(e)
+        quit()
+
+    with open('outputs/' + 'sillysearch' + rules + '.txt', "w") as file:
+        for offset in tqdm(offsets):
+            if offset != 1:
+                s = f"http://awbw.mooo.com/search?q={search}&offset={offset}"
+                # http://awbw.mooo.com/search?q=live+league+std+"ncghost12"&offset={offset}
+                # http://awbw.mooo.com/search?q=ncghost12&offset=201
+                # http://awbw.mooo.com/searchReplays.php?q=ncghost12
+                time.sleep(1)  # slows down searches on the mooo site so it doesn't get angi at me :>
+                page = page_getter(s)
+
+            table = page.find("div", class_="tableWrapper").find("table", class_="sortable").find("tbody")
+            for row in table.find_all('tr'):
+                items = row.find_all('td')
+                p1win = False
+                p2win = False
+                if len(items[5].attrs['class']) == 2:  # as the alternative to 'playerColumn'
+                    p2win = True
+                elif len(items[7].attrs['class']) == 2:
+                    p1win = True
+                w = 1 if p1win else (2 if p2win else 0)
+                for i, item in enumerate(items):
+                    if 4 <= i <= 8:
+                        match i:
+                            case 4:
+                                p1_co = str(item.attrs['data-sort'])
+                            case 6:
+                                p2_co = str(item.attrs['data-sort'])
+                            case 5:
+                                p1 = str(item.next.next)
+                            case 7:
+                                p2 = str(item.next.next)
+                            case 8:
+                                days = int(item.next)
+
+                # saving stuff
+                # days, winner 1/2/d, rating1, co1, rating2, co2
+                a = (f"{days};{w};"
+                     f"{int(p1[::-1].split('(')[0][::-1][:-1])};{p1_co};"
+                     f"{int(p2[::-1].split('(')[0][::-1][:-1])};{p2_co}")
+                file.write(a + "\n")
+
+
+def retrieve_silly(rules):
+    table = np.loadtxt('outputs/' + 'sillysearch' + rules + '.txt', delimiter=';', dtype=str)
+
+    days = np.zeros(table.shape[0])
+    winner = np.zeros(table.shape[0])
+    ratingw = np.zeros(table.shape[0])
+    ratingl = np.zeros(table.shape[0])
+    cow = [None] * table.shape[0]
+    col = [None] * table.shape[0]
+    for i, row in enumerate(table):
+        # days, winner 1/2/d, rating1, co1, rating2, co2
+        w = int(row[1])
+        if w == 0:  # remove draws completely
+            continue
+        if row[3] == row[5]:
+            continue  # removes mirrors completely
+        winner[i] = w
+        days[i] = int(row[0])
+
+        ratings = [0, row[2], row[4]]
+        cos = [0, row[3], row[5]]
+        ratingw[i] = int(ratings[w])
+        ratingl[i] = int(ratings[-w])
+        cow[i] = cos[w]
+        col[i] = cos[-w]
+
+    return days, winner, ratingw, ratingl, cow, col
 
 
 def extract_elo(s):
@@ -219,6 +386,52 @@ def extract_elo(s):
         tier[i] = row[3][2:]
         days[i] = int(row[4])
     return elo, date, oppelo, days, result, co_pick, co_against, tier
+
+
+def redo_sort(elo, date, oppelo, days, result, co_pick, co_against, tier, s):
+    # if s.split('+')[-1] == 'noname':  # todo?
+    #     return elo, date, oppelo, days, result, co_pick, co_against, tier
+    for i, e in enumerate(elo):
+        if i == 0:
+            continue
+        if result[i] == 1:
+            elodiff = 1  # win
+        elif result[i] == -1:
+            elodiff = 1  # loss
+        print(f'{elodiff} vs {e - elo[i - 1]}')
+        a = calc_elo_exchanged(elo, oppelo, result)  # wrong inputs ik
+    return elo, date, oppelo, days, result, co_pick, co_against, tier
+
+
+def calc_elo_exchanged(elo1, elo2, win, morethan30=True):
+    # calculates the player's (elo1) before a win, given elo after.
+    # inverted order to make sense of how to build it up
+    # elo1 = elo0 + exchange
+    # exchange = k * (1-frac)  # or opposite way round
+
+
+    k = 30  # 50 below 30 games played
+    searching = True
+    elo0 = 0
+    while searching:
+        frac = 1 / (1 + 10 ** (np.abs(elo1 - elo2) / 400))
+        if win:
+            exchange = k * frac
+        else:
+            exchange = k * (1 - frac)
+
+        if elo1 <= elo0 + exchange <= elo1:
+            searching = False
+        elif elo0 > 1e4:
+            return False  # this will get caught higher up
+        else:
+            elo0 += 1
+
+    # elonew = elo1 + higherwin  # example
+    # if win:
+    #     return higherwin
+    # return lowerwin
+    return None
 
 
 def scrape(search):
