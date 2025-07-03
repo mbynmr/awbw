f57520734ca443c98cda3945c0f207360f09af88
mbynmr <mbynmr@nottingham.ac.uk>
2025-07-03 01:00:56 +0100

    wha

--- customerrors.py
+++ customerrors.py
@@ -2,3 +2,4 @@ class CustomError(Exception):
     def __init__(self, message):
         super().__init__(message)
         # if I want extra arguments, go to https://stackoverflow.com/a/1319675
+        # todo just stick in other files? idk
