dd5365112ea19059a8cdd5e5285079a23242be48
mbynmr <mbynmr@nottingham.ac.uk>
2025-09-19 17:30:24 +0100

    plotter_alt

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # from GUI import GUI
 # from aimain import test
 from XHKO import calc
-from eloplot import plotter, silly_func
+from eloplot import plotter, plotter_alt, silly_func
 from pixelart import pixel_art
 from base64images import converter
 
 
 def main():
     # calc()
-    plotter()
+    # plotter()
+    plotter_alt()
     # silly_func()
     # pixel_art()
     # converter()
